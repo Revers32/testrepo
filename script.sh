6978160dfef68398502d9955a4b78b4627876d1f
Revers <kermode3222@gmail.com>
2021-02-05 17:07:53 +0200

    Updated script.sh

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ path=/var/log/scriptlog
 filname=script.log
 x=1
 
-mkdir $path
-touch $path/$filename
-chmod a+x $path/$filename
+mkdir -p $path
+[[ -f $filename ]] || touch $path/$filename
+[[ -d $filename ]] || chmod a+x $path/$filename
 
 while [ $x -lt 2 ]
 do
-echo 'Hello World' >> $path/script.log
+echo 'Hello World' >> $path/$filename
 x=$(($x-1))
 sleep 30
 done
