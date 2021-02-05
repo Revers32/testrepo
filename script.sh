cfedfe93ff586314bae107efc05d8bb3a9f94942
Revers32 <kermode3222@gmail.com>
2021-02-05 15:08:33 +0200

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+path=/var/log/scriptlog
+filepath=/var/log/scriptlog/script.log
+sudo mkdir $path
+sudo touch $filepath
+sudo chmod 777 $filepath
 x=1
 while [ $x -lt 2 ]
 do
-echo 'Hellow World' >> 123.log
+echo 'Hellow World' >> $path/script.log
 x=$(($x-1))
+sleep 30
 done
