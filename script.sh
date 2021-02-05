30fe8feb1533d28f2f8df507c17a14b98bbd79b7
Revers <kermode3222@gmail.com>
2021-02-05 16:31:44 +0200

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 path=/var/log/scriptlog
-filepath=/var/log/scriptlog/script.log
-sudo mkdir $path
-sudo touch $filepath
-sudo chmod 777 $filepath
+filname=script.log
 x=1
+
+mkdir $path
+touch $path/$filename
+chmod a+x $path/$filename
+
 while [ $x -lt 2 ]
 do
 echo 'Hello World' >> $path/script.log
