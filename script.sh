3d7bc38aff99a0fab2b02d1ad1692fffb3a885ea
Revers32 <kermode3222@gmail.com>
2021-02-12 10:30:42 +0200

    Added named parameters

--- script.sh
+++ script.sh
@@ -4,18 +4,21 @@ set -o nounset
 
 path=/var/log/scriptlog
 filename=script.log
-x=1
-
 
 mkdir -p $path
 [[ -f $filename ]] || touch $path/$filename
 [[ -d $filename ]] || chmod a+x $path/$filename
 
-while true; do
-case $x in
-	1)sleepTime=$1;;
-	2)text=$2;;
-	*)echo $text >> $path/$filename && sleep $sleepTime;;
+while true;do
+case $1 in
+	-s | --sleeptime)
+	sleep="$2"
+	shift
+	shift;;
+	-t | --text)
+	text="$2"
+	shift
+	shift;;
+	-r | --run )echo $text >> $path/$filename && sleep $sleep;;
 esac
-x=$(($x+1))
 done
