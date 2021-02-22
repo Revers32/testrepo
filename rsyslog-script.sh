ebec231eb1a55bb1c8a5528f8ad249fc473f53cb
root <root@ip-172-31-39-98.us-east-2.compute.internal>
2021-02-22 11:15:00 +0000

    Added rsyslog-script and configuration file for rsyslog

---
+++ rsyslog-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+while [[ -n "${1:-}" ]]
+do
+case "$1" in
+	-s | --sleeptime) sleep="$2"
+	shift;;
+	-t | --text) text="$2"
+	shift;;
+	*) echo "( $1 ) Option not found"
+	exit 1;; 
+esac
+shift
+done
+
+while true; do
+exec > >(tee >(logger  -p local0.notice "$text" ))
+sleep $sleep
+done
