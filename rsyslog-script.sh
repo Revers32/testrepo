b74edfd1210f31faf5ad8b683b75c1380b1db0e1
Revers32 <kermode3222@gmail.com>
2021-03-04 10:41:02 +0000

    Added rsyslog-script.sh

---
+++ rsyslog-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+path=/var/log
+filename=script.log
+
+mkdir -p $path
+[[ -f $filename ]] || touch $path/$filename && chmod a+x $path/$filename
+
+while [[ -n "${1:-}" ]]
+do
+case "$1" in
+	-s | --sleeptime) sleep="$2"
+	shift;;
+	-t | --text) text="$2"
+	shift;;
+	*) echo "( $1 ) Option not found"
+	exit 1;; 
+esac
+shift
+done
+
+while true; do
+logger  -p local0.notice "$text"
+sleep $sleep
+done
